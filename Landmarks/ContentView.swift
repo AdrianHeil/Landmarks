aac2d94360ba5a72351cfddf8ca6033662234325
Adrian <a.heil@outlook.com>
2020-07-13 21:50:22 +0200

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Adrian Heil on 13/07/20.
+//  Copyright © 2020 Adrian Heil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
